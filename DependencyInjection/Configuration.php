3943d904e6f3ee3b17efbfcb5dfdb272a9bee63a
Harouna Madi <harouna.madi@fidesio.com>
2016-02-29 14:45:06 +0100

    refactoring config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,9 +20,17 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('fidesio_isidore');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode('client')
+                    ->children()
+                        ->scalarNode('url')->end()
+                        ->scalarNode('login')->end()
+                        ->scalarNode('password')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
