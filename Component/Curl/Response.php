9c238e334e9d921f598834124d916252366460a4
Felix SOEDJEDE <felix.soedjede@fidesio.com>
2019-08-29 18:43:16 +0200

    Fix Response when content is false

--- Component/Curl/Response.php
+++ Component/Curl/Response.php
@@ -24,30 +24,31 @@ final class Response extends sfResponse
      *
      * @throws \InvalidArgumentException When the HTTP status code is not valid
      */
-    public function __construct($content = '', $status = self::HTTP_OK, array $headers = [])
+    public function __construct($content, $status = self::HTTP_OK, array $headers = [])
     {
-        if (!empty($content)) {
+        if (empty($content)) {
+            $content = '';
+        } else {
             $res = explode("\r\n\r\n", $content);
-
             $rawHeaders = explode("\r\n", $res[0]);
 
             if (isset($rawHeaders[0]) && preg_match('@\s(\d{3})\s@', $rawHeaders[0], $matches)) {
                 $status = (int)$matches[1];
             }
 
-            $protocolVersion = '1.' . ((isset($rawHeaders[0]) && preg_match('@HTTP\/1\.([0-1]{1})@si', $rawHeaders[0], $m)) ? $m[1] : '0');
+            $headers = array_merge($headers, $this->parseRawHeaders($rawHeaders));
+            $content = $res[1];
+        }
 
-            $headers = $this->parseRawHeaders($rawHeaders);
+        parent::__construct($content, $status, $headers);
 
-            $rawContent = $res[1];
-
-            parent::__construct($rawContent, $status, $headers);
-
+        if (isset($headers['Content-Type']) && $headers['Content-Type'] === 'application/json') {
+            $protocolVersion = sprintf(
+                '1.%s',
+                (isset($rawHeaders[0]) && preg_match('@HTTP\/1\.([0-1]{1})@si', $rawHeaders[0], $m)) ? $m[1] : '0'
+            );
             $this->setProtocolVersion($protocolVersion);
-
-            if (isset($headers['Content-Type']) && $headers['Content-Type'] == 'application/json') {
-                $this->data = json_decode($rawContent, true);
-            }
+            $this->data = json_decode($content, true);
         }
     }
 
